88d2a59cba4acf268a900f3e2ad50d13caa0d927
Zewws <bukharoff.iwan@yandex.ru>
2022-07-13 13:39:39 +0500

    Program added

---
+++ Word Generator/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Word_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сгенерировать слово";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(23, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(308, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Location = new System.Drawing.Point(359, 36);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(52, 30);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(344, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 47);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Кол-во слогов";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 185);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Word Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Word Generator/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Word_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Random r = new Random(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string SylGen()
+        {
+            string syl = "";
+            string vow = "EYUIOA";
+            string con = "QWRTPSDFGHJKLZXCVBNM";
+            int wf;
+            int lc = r.Next(1, 4);
+            switch (lc)
+            {
+                case 1:
+                    syl += vow[r.Next(0, vow.Length)];
+                    break;
+                case 2:
+                    wf = r.Next(1, 2);
+                    if (wf == 1) 
+                    {
+                        syl += con[r.Next(0, con.Length)];
+                        syl += vow[r.Next(0, vow.Length)];
+                    }
+                    else
+                    {
+                        syl += vow[r.Next(0, vow.Length)];
+                        syl += con[r.Next(0, con.Length)];
+                    }
+                    break;
+                case 3:
+                    syl += con[r.Next(0, con.Length)];
+                    syl += vow[r.Next(0, vow.Length)];
+                    syl += con[r.Next(0, con.Length)];
+                    break;
+                case 4:
+                    syl += con[r.Next(0, con.Length)];
+                    wf = r.Next(1, 2);
+                    if (wf == 1)
+                    {
+                        syl += con[r.Next(0, con.Length)];
+                        syl += vow[r.Next(0, vow.Length)];
+                    }
+                    syl += con[r.Next(0, con.Length)];
+                    break;
+            }
+            return syl;
+        }
+
+        public string StrGen(int sylc)
+        {
+            string s = "";
+            for (int i=0; i<sylc; i++) { s = s+SylGen(); }
+            return s;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = StrGen(Convert.ToInt32(numericUpDown1.Value));
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
